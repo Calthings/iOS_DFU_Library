307b6fae22067f7193feb2e9f9b0b9aefea59ef6
Sava <savasavic100@gmail.com>
2021-06-11 15:45:35 +0200

    IOS-4703: Fix compilation of DFU Library with iOS SDK 15

--- iOSDFULibrary/Classes/Implementation/LegacyDFU/Services/LegacyDFUService.swift
+++ iOSDFULibrary/Classes/Implementation/LegacyDFU/Services/LegacyDFUService.swift
@@ -156,13 +156,13 @@ import CoreBluetooth
         let peripheral = service.peripheral
         
         // Set the peripheral delegate to self
-        peripheral.delegate = self
+        peripheral?.delegate = self
         
         // Discover DFU characteristics
         logger.v("Discovering characteristics in DFU Service...")
         logger.d("peripheral.discoverCharacteristics(nil, for: \(uuidHelper.legacyDFUService.uuidString))")
         
-        peripheral.discoverCharacteristics(nil, for: service)
+        peripheral?.discoverCharacteristics(nil, for: service)
     }
     
     /**
@@ -192,8 +192,7 @@ import CoreBluetooth
         // - we must be in the DFU mode already (otherwise the device would be useless...).
         // Note: On iOS the Generic Access and Generic Attribute services (nor HID Service)
         //       are not returned during service discovery.
-        let services = service.peripheral.services!
-        if services.count == 1 {
+        if let services = service.peripheral?.services, services.count == 1 {
             return false
         }
         // If there are more services than just DFU Service, the state is uncertain.
